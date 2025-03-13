05816c3b5b7320bafb6207619436fea9c1fc1125
LCaseiro <lcaseiro@eneida.io>
2025-03-13 15:10:31 +0000

    added serial print

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -27,12 +27,15 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LEDpin, OUTPUT);
+  SerialBegin(9600);
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  SerialPrint("ON");
   digitalWrite(LEDpin, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(1000);                      // wait for a second
+  SerialPrint("OFF");
   digitalWrite(LEDpin, LOW);   // turn the LED off by making the voltage LOW
   delay(1000);                      // wait for a second
 }
